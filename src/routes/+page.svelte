47d8aab6d167f67709df1be1d1aa8a70d4af1fc2
B1tzzzzz <matew651@gmail.com>
2024-11-08 22:20:38 +0300

    Вершина двигается, норм выглядит

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import Draggable from "./draggable.svelte";
+    import DraggableTxt from "./draggableTxt.svelte";
+    enum Direction
+    {
+        up = 1, down = -1
+    }
+
+    
+    let name:string = "asd"
+    
+</script>
+
+<Draggable>
+    <DraggableTxt>
+    </DraggableTxt>
+</Draggable>
+
+<h1>Welcome to {name}, {Direction.up}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ src/routes/draggable.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let left = 100;
+	export let top = 100;
+	
+	let moving = false;
+	
+	function onMouseDown() {
+		moving = true;
+	}
+	
+	/**
+     * @param {{ movementX: number; movementY: number; }} e
+     */
+	function onMouseMove(e) {
+		if (moving) {
+			left += e.movementX;
+			top += e.movementY;
+		}
+	}
+	
+	function onMouseUp() {
+		moving = false;
+	}
+	
+// 	$: console.log(moving);
+</script>
+
+<style>
+	.draggable {
+        
+        background-color: rgb(23, 23, 23);
+		user-select: none;
+		cursor: move;
+		border: solid 7px rgb(49, 49, 49);
+        border-radius: 50%;
+        height:50px;
+        width:50px;
+        -moz-border-radius:50%;
+        -webkit-border-radius:50%;
+		position: absolute;
+	}
+
+</style>
+
+<section on:mousedown={onMouseDown} style="left: {left}px; top: {top}px;" class="draggable">
+	<slot></slot>
+</section>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
