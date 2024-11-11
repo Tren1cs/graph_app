17c8d2474bd3bbb074ad34c69b4da5e5f6c84f93
Tren1cs <93768311+Tren1cs@users.noreply.github.com.>
2024-11-11 21:36:33 +0300

    Поставил shadcn, начал что-то делать
    Пожалуйста, просто сделайте рендеринг графа в файле editor_tests/graphView.svelte. Этот компонент я импортирую на главной странице сайта. Пока что я сделал чекбоксы и начал делать страницу редактора.

---
+++ src/routes/editor_tests/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import GraphView from "./graphView.svelte";
+    import Checkbox from "$lib/components/ui/checkbox/checkbox.svelte";
+
+    let settings = $state({
+        GraphName: "Graph 1",
+        View: {
+            physics: false
+        },
+        Edit: {
+            directed: false
+        }
+    });
+    $inspect(settings.View.physics)
+</script>
+
+<div class=" bg-black h-screen w-screen m-0 text-white">
+    <GraphView />
+    <div class=" h-full w-full z-10 m-0 p-8 absolute">
+        <div class=" bg-zinc-900 rounded-xl w-full h-14"></div>
+        <div class="bg-zinc-950 rounded-xl w-fit h-fit p-5 mt-5">
+            <h1 class="text-xl mb-3">{settings.GraphName}</h1>
+            <div class="bg-zinc-900 my-3 p-4 rounded-lg w-96">
+                <h2 class="text-lg">View options</h2>
+                <div class="text-sm flex flex-row p-0 my-3 w-full">
+                    <p class="w-fit text-nowrap">Enable physics</p>
+                    <div class=" w-full"></div>
+                    <Checkbox class="border-zinc-700 hover:bg-zinc-800 transition-all duration-150" bind:checked={settings.View.physics}/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
---
+++ src/routes/editor_tests/graphView.svelte
@@ -0,0 +1,3 @@
+<div class=" bg-transparent h-full w-full text-white top-0 left-0 text-center content-center z-0 absolute m-0">
+    *This component is supposed to be graph view*
+</div>
