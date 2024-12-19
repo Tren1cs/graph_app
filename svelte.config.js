22aa491c187d720b65a6f6d87a2f0666ff22b72d
Tren1cs <93768311+Tren1cs@users.noreply.github.com.>
2024-12-19 23:05:00 +0300

    fuck this

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,10 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
+	preprocess: vitePreprocess({ script: true }),
+
 	kit: {
 		adapter: adapter({
 			fallback: '404.html',
