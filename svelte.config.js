c0ee059a38700b3c0af24e79570cf4282e951e0f
Tren1cs <93768311+Tren1cs@users.noreply.github.com>
2024-12-19 21:52:52 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,11 @@ const dev = process.argv.includes('dev');
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
