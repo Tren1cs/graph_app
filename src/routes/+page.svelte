81ff4e963ef99b4cd0e7b63303875255d97c9051
Tren1cs <93768311+Tren1cs@users.noreply.github.com>
2025-04-24 19:42:47 +0300

    приветствие

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,6 +74,10 @@
                     <!--<input class="mb-4 border-none text-xl bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-fit w-full rounded-md border p-0 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" bind:value={settings.GraphName} placeholder="Graph name"/>-->
                     <!--<AlgoMenu {algo}></AlgoMenu>-->
                     <h1 class="text-2xl">Добро пожаловать!</h1>
+                    <p class="text-lg">
+                        Это редактор графов. Здесь вы можете рисовать графы и экспортировать их в понятном программному коду виде.
+                        Для начала работы нажмите на кнопку "Гайд" в правом верхнем углу экрана!
+                    </p>
                 </Resizable.Pane>
                 <Resizable.Handle class="z-50" />
                 <Resizable.Pane class="z-0">
